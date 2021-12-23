c5628fb83a063f5a0e2c944dd21b9556406b7272
avani9799 <avani.s.addweb@gmail.com>
2021-12-23 16:58:11 +0530

    Weather Submitted

---
+++ NodeJS/WeatherApp/config.js
@@ -0,0 +1,8 @@
+const constants={
+    openWeatherMap:{
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "8692497ffdfe4a7a496889d94392b26f"
+    }
+}
+
+module.exports=constants;
